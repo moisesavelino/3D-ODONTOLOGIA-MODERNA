cbfb3e92af88af13b0d0a4a96c7feb9716680fa3
Instituto Moisés Avelino <105991902+moisesavelino@users.noreply.github.com>
2025-12-23 01:04:15 -0300

    Add Login component for user authentication

---
+++ frontend/ src/pages/Login.jsx
@@ -0,0 +1,10 @@
+export default function Login(){
+ return(
+  <div>
+   <h1>3D Odontologia Moderna</h1>
+   <input placeholder='Usuário'/>
+   <input type='password' placeholder='Senha'/>
+   <button>Entrar</button>
+  </div>
+ )
+}
